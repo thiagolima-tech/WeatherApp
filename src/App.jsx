a5e8ef302308352f6f8684c3e3f7be58b3e44198
Vitor <vitoralcan5@gmail.com>
2025-07-29 22:50:03 -0300

    Ajuste do merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Sidebar from "./Components/Sidebar/SideBtn";
 
 function App() {
   return (
-    <PaginaInicial 
-    regiao="Sudeste"
-    temperatura="10º"
-    placeholder="Digite o estado que você deseja saber mais..."
-    />
+    <>
+      <PaginaInicial
+        regiao="Sudeste"
+        temperatura="10º"
+        placeholder="Digite o estado que você deseja saber mais..."
+      />
+      <Sidebar />
+    </>
   )
-} 
+}
 export default App;
