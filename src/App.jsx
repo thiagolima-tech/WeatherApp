f57e7daf902c54d8eb57228190d279f7e19a840a
Vitor <vitoralcan5@gmail.com>
2025-07-29 22:43:36 -0300

    Pagina inicial completa, só falta as funcçoes dos btn e adicionar a tipografia

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import PaginaInicial from "./Components/PaginaInicial/PaginaInicial";
 
 function App() {
   return (
-    <PaginaInicial />
+    <PaginaInicial 
+    regiao="Sudeste"
+    temperatura="10º"
+    placeholder="Digite o estado que você deseja saber mais..."
+    />
   )
-}
+} 
 export default App;
--- src/Components/PaginaInicial/PaginaInicial.jsx
+++ src/Components/PaginaInicial/PaginaInicial.jsx
@@ -1,19 +1,26 @@
 import { WiNightCloudyWindy } from "react-icons/wi";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import backgroundCeu from '../../assets/imagemCeu.png'
 
 const PaginaInicial = ( {regiao, temperatura, placeholder} ) => {
     return (
-        <div
-            className="bg-cover bg-center h-screen flex flex-col text-center items-center justify-center"
+        <section
+            className="bg-cover bg-center bg-no-repeat h-screen sm:hidden"
             style={{ backgroundImage: `url(${backgroundCeu})` }}
         >
-            <div className="m-10">
-                <WiNightCloudyWindy className="text-[180px] text-gray-700" />
-                <h2>{regiao}</h2>
-                <h2>{temperatura}</h2>
-                <input className="p-2 bg-white" type="search" placeholder={placeholder}/>
+            <div className="flex mx-1 text-gray-800 text-3xl justify-between relative items-center top-[50%]">
+                <IoIosArrowBack />
+                <IoIosArrowForward />
             </div>
-        </div>
+            <div className="flex flex-col text-center items-center">
+                <WiNightCloudyWindy className="text-center text-[180px] text-gray-700 mt-40" />
+                <h2 className="text-[40px] text-gray-100">{regiao}</h2>
+                <h2 className="text-[150px] text-gray-100">{temperatura}</h2>
+                <input className="py-2 px-10 bg-white rounded-2xl" 
+                    type="search"
+                    placeholder={placeholder}/>
+            </div>
+        </section>
     )
 }
 
