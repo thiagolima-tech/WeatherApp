76e2d3d1e7d0df79c53ee74f08e61cdf1f1990fa
Davi Munhoz <davimunhoz1005@gmail.com>
2025-08-02 10:56:44 -0300

    feat(cities-temperature): create page temperature from an state showing cities

--- src/pages/PaginaInicial/PaginaInicial.jsx
+++ src/pages/PaginaInicial/PaginaInicial.jsx
@@ -2,20 +2,22 @@ import { WiNightCloudyWindy } from "react-icons/wi";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import backgroundCeu from '../../assets/imagemCeu.png'
 
-const PaginaInicial = ( {regiao, temperatura, placeholder} ) => {
+const PaginaInicial = ( {regiao, temperatura, placeholder, onClick} ) => {
     return (
         <section
-            className="bg-cover bg-center bg-no-repeat h-screen sm:hidden"
+            className="bg-cover bg-center bg-no-repeat h-screen"
             style={{ backgroundImage: `url(${backgroundCeu})` }}
         >
             <div className="flex mx-1 text-gray-800 text-3xl justify-between relative items-center top-[50%]">
-                <IoIosArrowBack />
-                <IoIosArrowForward />
+                <IoIosArrowBack cursor="pointer"/>
+                <IoIosArrowForward cursor="pointer"/>
             </div>
-            <div className="flex flex-col text-center items-center">
-                <WiNightCloudyWindy className="text-center text-[180px] text-gray-700 mt-40" />
+            <div className="flex flex-col items-center cursor-pointer" onClick={onClick}>
+                <WiNightCloudyWindy className="text-center text-[180px] text-gray-800 mt-40" />
                 <h2 className="text-[40px] text-gray-100">{regiao}</h2>
                 <h2 className="text-[150px] text-gray-100">{temperatura}</h2>
+            </div>
+            <div className="flex text-center justify-center">
                 <input className="py-2 px-10 bg-white rounded-2xl" 
                     placeholder={placeholder}/>
             </div>
