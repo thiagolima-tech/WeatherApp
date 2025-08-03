463749c48e02640647a82804f1bfbaab73ce5bfc
Davi Munhoz <davimunhoz1005@gmail.com>
2025-08-03 13:15:41 -0300

    feat(btn-precious-page): add an arrow to go back to precious page in page StateCities

--- src/pages/StateCities.jsx
+++ src/pages/StateCities.jsx
@@ -1,9 +1,20 @@
-import { WiNightCloudyWindy } from "react-icons/wi";
+import { WiDirectionLeft, WiNightCloudyWindy } from "react-icons/wi";
 import CityWeatherCard from "../Components/CityWeatherCard";
+import { useNavigate } from "react-router-dom";
 
 function StateCities() {
+    const navigate = useNavigate();
+
+    function handleClickBack(event) {
+        event.preventDefault();
+        navigate(-1);
+    }
+
     return (
-        <div>
+        <div className="relative">
+            <div className="absolute top-2 z-10" onClick={handleClickBack}>
+                <WiDirectionLeft size={50} className="cursor-pointer text-gray-700" />
+            </div>
             <div className="flex flex-col items-center">
                 <WiNightCloudyWindy className="text-center text-8xl text-gray-800" />
                 <h1 className="text-2xl">Nordeste</h1>
